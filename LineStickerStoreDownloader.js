2f9dddafab90b29bd4e3ed1e6c0058f28ad9249d
Sherry <sherryyue.c@protonmail.com>
2022-12-28 02:50:18 +0800

    新增腳本：Line Store 貼圖下載器

---
+++ LineStickerStoreDownloader.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name            Line Store Sticker Downloader
+// @name:zh-TW      Line Store 貼圖下載器
+// @namespace       com.sherryyue.linestickerstoredownloader
+// @version         0.1
+// @description       Line Store Sticker Downloader.
+// @description:ZH-TW Line Store 貼圖下載器。
+// @author          SherryYue
+// @match           https://store.line.me/*
+// @supportURL      sherryyue.c@protonmail.com
+// @icon            https://sherryyuechiu.github.io/card/images/logo/maskable_icon_x96.png
+// @require         https://code.jquery.com/jquery-3.6.0.js
+// @require         https://code.jquery.com/ui/1.13.1/jquery-ui.js
+// @grant           GM_addStyle
+// ==/UserScript==
+
+(function () {
+  let downloadImage = (url) => {
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  let main = () => {
+    $('.FnStickerPreviewItem').click(function(){
+      let data = JSON.parse($(this).attr('data-preview'));
+      let clearImageUrl = data.fallbackStaticUrl;
+      downloadImage(clearImageUrl);
+    })
+  }
+
+  let observer = new MutationObserver((mutations, obs) => {
+    if (!document.querySelector(".FnStickerPreviewItem")) return;
+    main();
+    observer.disconnect();
+  });
+
+  observer.observe(document.querySelector("body"), {
+    childList: true,
+    subtree: true
+  });
+
+  document.getElementsByTagName('head')[0].append(
+    '<link '
+    + 'href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css" '
+    + ' type="text/css">'
+  );
+})();

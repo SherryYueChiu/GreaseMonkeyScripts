b7700808380253bdfeccca7961b39bc202031d7c
Sherry <sherryyue.c@protonmail.com>
2023-05-12 02:27:53 +0800

    更新腳本：交通違規檢舉輸入助手

--- TrafficFineReportAssist.js
+++ TrafficFineReportAssist.js
@@ -2,11 +2,12 @@
 // @name            TrafficFineReportAssist
 // @name:zh-TW      交通違規檢舉輸入助手
 // @namespace       com.sherryyue.TrafficFineReportAssist
-// @version         0.2
+// @version         0.3
 // @description     交通違規檢舉輸入助手
 // @author          SherryYue
 // @copyright       SherryYue
 // @match           *://suggest.police.taichung.gov.tw/*
+// @match           *://jiaowei.ncpb.gov.tw/sc11/*
 // @contributionURL https://sherryyuechiu.github.io/card
 // @supportURL      sherryyue.c@protonmail.com
 // @icon            https://sherryyuechiu.github.io/card/images/logo/maskable_icon_x96.png
@@ -22,7 +23,6 @@
     MALE: 1,
     FEMALE: 2,
   }
-
   const profile = {
     fullName: '',
     gender: GENDER.FEMALE,
@@ -32,39 +32,81 @@
     mail: '',
   }
 
-  let feild = {};
-  feild.disclaimerRead = document.querySelector('#OK');
-  feild.fullName = document.querySelector('#name');
-  feild.genderMale = document.querySelector('#male');
-  feild.genderFemale = document.querySelector('#female');
-  feild.nation = document.querySelector('#taiwan');
-  feild.id = document.querySelector('#sub');
-  feild.addr = document.querySelector('#address');
-  feild.tel = document.querySelector('#liaisontel');
-  feild.mail = document.querySelector('#email');
-  feild.actSelect = document.querySelector('#qclass')
-  feild.dateTime = document.querySelector('#violationdatetime');
-  feild.detail = document.querySelector('#detailcontent')
+  function taichung() {
+    let field = {};
 
-  const urlPathName = location.pathname;
-  if (urlPathName === '/traffic/' || urlPathName === '/traffic/index.jsp') {
-    feild.disclaimerRead.click();
-    feild.disclaimerRead.click();
-    feild.disclaimerRead.checked = true;
-  } else if (urlPathName === '/traffic/traffic_write.jsp') {
-    feild.fullName.value = profile.fullName;
-    if (profile.gender === GENDER.FEMALE) feild.genderFemale.click();
-    else if (profile.gender === GENDER.MALE) feild.genderMale.click();
-    feild.nation.click();
-    feild.id.value = profile.id;
-    feild.addr.value = profile.addr;
-    feild.tel.value = profile.tel;
-    feild.mail.value = profile.mail;
-    feild.dateTime.removeAttribute('readonly');
-    // 依下拉選單自動填入描述
-    feild.actSelect.onchange = () => {
-      feild.detail.value = feild.actSelect.value.replace(/^道交[\d-、之第項]+/gi, '');
+    const urlPathName = location.pathname;
+    if (urlPathName === '/traffic/' || urlPathName === '/traffic/index.jsp') {
+      field.disclaimerRead = document.querySelector('#OK');
+
+      field.disclaimerRead.click();
+      field.disclaimerRead.click();
+      field.disclaimerRead.checked = true;
+    } else if (urlPathName === '/traffic/traffic_write.jsp') {
+      field.fullName = document.querySelector('#name');
+      field.genderMale = document.querySelector('#male');
+      field.genderFemale = document.querySelector('#female');
+      field.nation = document.querySelector('#taiwan');
+      field.id = document.querySelector('#sub');
+      field.addr = document.querySelector('#address');
+      field.tel = document.querySelector('#liaisontel');
+      field.mail = document.querySelector('#email');
+      field.actSelect = document.querySelector('#qclass');
+      field.dateTime = document.querySelector('#violationdatetime');
+      field.detail = document.querySelector('#detailcontent');
+      
+      field.fullName.value = profile.fullName;
+      if (profile.gender === GENDER.FEMALE) field.genderFemale.click();
+      else if (profile.gender === GENDER.MALE) field.genderMale.click();
+      field.nation.click();
+      field.id.value = profile.id;
+      field.addr.value = profile.addr;
+      field.tel.value = profile.tel;
+      field.mail.value = profile.mail;
+      field.dateTime.removeAttribute('readonly');
+      // 依下拉選單自動填入描述
+      field.actSelect.onchange = () => {
+        field.detail.value = field.actSelect.value.replace(/^道交[\d-、之第項]+/gi, '');
+      }
     }
   }
 
+  function nanTou() {
+    let field = {};
+
+    const urlPathName = location.pathname;
+    if (urlPathName === '/sc11/rwd/rincase1.aspx') {
+      field.nextBtn = document.querySelector('#Button1');
+      field.nextBtn.click();
+    } else if (urlPathName === '/sc11/rwd/rincase2.aspx') {
+      window.scrollTo(0, document.body.scrollHeight);
+    } else if (urlPathName === '/sc11/rwd/rincase3.aspx') {
+      field.fullName = document.querySelector('#mname');
+      field.id = document.querySelector('#mpid');
+      field.tel = document.querySelector('#mtel');
+      field.addr = document.querySelector('#maddr');
+      field.mail = document.querySelector('#memail');
+      field.actSelect = document.querySelector('#rlid');
+      field.detail = document.querySelector('#mcarblack');
+
+      field.fullName.value = profile.fullName;
+      field.id.value = profile.id;
+      field.tel.value = profile.tel;
+      field.addr.value = profile.addr;
+      field.mail.value = profile.mail;
+      // 依下拉選單自動填入描述
+      field.actSelect.onchange = () => {
+        // 連續的option可能是同一條
+        const option = field.actSelect.querySelectorAll(`option[value='${field.actSelect.value}']`);
+        const content = [...option].map(elm => elm.textContent).join('');
+        field.detail.value = content.replace(/^[\d-、之第條項款]+/gi, '').replace(/\([\d-、之第條項款]+\)$/gi, '');
+      }
+    }
+  }
+
+  if (location.host === 'suggest.police.taichung.gov.tw') {
+    taichung();
+  } else if (location.host === 'jiaowei.ncpb.gov.tw') {
+    nanTou();
+  }
 })();

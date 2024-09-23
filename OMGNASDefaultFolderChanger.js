cb7519b27ba7f28703c3a45f09eab642dce57133
Sherry <sherryyue.c@protonmail.com>
2024-09-23 16:09:17 +0800

    UPDATE：OMG NAS 自動點開常用資料夾

--- OMGNASDefaultFolderChanger.js
+++ OMGNASDefaultFolderChanger.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name:zh-tw      OMG NAS 自訂預設資料夾
-// @name            OMG NAS Default Folder Changer
+// @name:zh-tw      OMG NAS 自動點開常用資料夾
+// @name            OMG NAS Auto open default folder
 // @namespace       com.sherryyue.omgnasdefaultfolderchanger
-// @version         0.1
+// @version         0.2
 // @description:zh-tw   開啟NAS後自動點開對內共用區
 // @description         Automatically open the folder "對內共用區" after opening the NAS
 // @author          SherryYue
@@ -18,14 +18,22 @@
 // @require         http://code.jquery.com/jquery-3.4.1.min.js
 // @require         https://greasyfork.org/scripts/383527-wait-for-key-elements/code/Wait_for_key_elements.js?version=701631
 // @grant           none
+// @downloadURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Default%20Folder%20Changer.user.js
+// @updateURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Default%20Folder%20Changer.meta.js
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    const query = '[ext\\:tree-node-id="remote\\/對內共用區"]';
-    waitForKeyElements(query, () => {
-        document.querySelector(query).click();
-        console.log('對內共用區已幫您自動點開');
+    const fileStation = '#ext-gen135';
+    waitForKeyElements(fileStation, () => {
+        document.querySelector(fileStation).click();
+        console.log('已幫您自動點開「File station」');
+
+        const folder = '[ext\\:tree-node-id="remote\\/對內共用區"]';
+        waitForKeyElements(folder, () => {
+            document.querySelector(folder).click();
+            console.log('已幫您自動點開「對內共用區」');
+        });
     });
 })();

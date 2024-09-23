ff1aebd7fccd6171e10579f23ab702e279fe601c
Sherry <sherryyue.c@protonmail.com>
2024-09-23 16:51:24 +0800

    UPDATE：OMG NAS 自動點開常用資料夾
    設定招財聚寶的常用路徑

--- OMGNASDefaultFolderChanger.js
+++ OMGNASDefaultFolderChanger.js
@@ -2,7 +2,7 @@
 // @name:zh-tw      OMG NAS 自動點開常用資料夾
 // @name            OMG NAS Auto open default folder
 // @namespace       com.sherryyue.omgnasdefaultfolderchanger
-// @version         0.2
+// @version         0.3
 // @description:zh-tw   開啟NAS後自動點開對內共用區
 // @description         Automatically open the folder "對內共用區" after opening the NAS
 // @author          SherryYue
@@ -18,8 +18,8 @@
 // @require         http://code.jquery.com/jquery-3.4.1.min.js
 // @require         https://greasyfork.org/scripts/383527-wait-for-key-elements/code/Wait_for_key_elements.js?version=701631
 // @grant           none
-// @downloadURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Default%20Folder%20Changer.user.js
-// @updateURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Default%20Folder%20Changer.meta.js
+// @downloadURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Auto%20open%20default%20folder.user.js
+// @updateURL https://update.greasyfork.org/scripts/507753/OMG%20NAS%20Auto%20open%20default%20folder.meta.js
 // ==/UserScript==
 
 (function () {
@@ -30,10 +30,22 @@
         document.querySelector(fileStation).click();
         console.log('已幫您自動點開「File station」');
 
-        const folder = '[ext\\:tree-node-id="remote\\/對內共用區"]';
-        waitForKeyElements(folder, () => {
-            document.querySelector(folder).click();
+        const folder0 = '[ext\\:tree-node-id="remote\\/對內共用區"]';
+        waitForKeyElements(folder0, () => {
+            document.querySelector(folder0).children[1].click();
             console.log('已幫您自動點開「對內共用區」');
+
+            // const folder1 = '[ext\\:tree-node-id="remote/對內共用區/1018S-招財聚寶"]';
+            // waitForKeyElements(folder1, () => {
+            //     document.querySelector(folder1).children[1].click();
+            //     console.log('已幫您自動點開「1018S」');
+
+            //     const folder2 = '[ext\\:tree-node-id="remote/對內共用區/1018S-招財聚寶/移交給程式檔"]';
+            //     waitForKeyElements(folder2, () => {
+            //         document.querySelector(folder2).click();
+            //         console.log('已幫您自動點開「移交給程式檔」');
+            //     });
+            // });
         });
     });
 })();

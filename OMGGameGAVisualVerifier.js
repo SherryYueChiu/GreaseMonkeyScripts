75ce7957fd95d3bd20b28f0f63d24b7c5683f006
Sherry <sherryyue.c@protonmail.com>
2024-10-18 17:17:26 +0800

    UPDATET：OMG遊戲GA視覺驗證工具
    網域配對變更

--- OMGGameGAVisualVerifier.js
+++ OMGGameGAVisualVerifier.js
@@ -1,106 +1,107 @@
-    // ==UserScript==
-    // @name:zh-tw      OMG遊戲GA視覺驗證工具
-    // @name            OMG Game GA Visual Verifier
-    // @namespace       com.sherryyue.omggamespeedcontroller
-    // @version         0.3
-    // @description:zh-tw       在畫面上toast出剛被記錄的 dataLayer 事件，方便驗證 GA 的事件追蹤
-    // @description             Toast the latest dataLayer event on the screen for verifying GA event tracking
-    // @author          SherryYue
-    // @copyright       SherryYue
-    // @license         MIT
-    // @match           *://*.ssgaka.com/*
-    // @exclude         *://*.ssgaka.com/history/*
-    // @exclude         *://*.ssgaka.com/history2/*
-    // @exclude         *://*.ssgaka.com/review/*
-    // @contributionURL https://sherryyuechiu.github.io/card
-    // @supportURL      sherryyue.c@protonmail.com
-    // @icon            https://sherryyuechiu.github.io/card/images/logo/maskable_icon_x96.png
-    // @supportURL      "https://github.com/sherryyuechiu/GreasyMonkeyScripts/issues"
-    // @homepage        "https://github.com/sherryyuechiu/GreasyMonkeyScripts"
-    // @require         https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.js
-    // @grant           none
-    // ==/UserScript==
+// ==UserScript==
+// @name:zh-tw      OMG遊戲GA視覺驗證工具
+// @name            OMG Game GA Visual Verifier
+// @namespace       com.sherryyue.omggamespeedcontroller
+// @version         0.4
+// @description:zh-tw       在畫面上toast出剛被記錄的 dataLayer 事件，方便驗證 GA 的事件追蹤
+// @description             Toast the latest dataLayer event on the screen for verifying GA event tracking
+// @author          SherryYue
+// @copyright       SherryYue
+// @license         MIT
+// @match           *://*.ssgaka.com/*
+// @match           *://7sz*.com/*
+// @exclude         *://*/history/*
+// @exclude         *://*/history2/*
+// @exclude         *://*/review/*
+// @contributionURL https://sherryyuechiu.github.io/card
+// @supportURL      sherryyue.c@protonmail.com
+// @icon            https://sherryyuechiu.github.io/card/images/logo/maskable_icon_x96.png
+// @supportURL      "https://github.com/sherryyuechiu/GreasyMonkeyScripts/issues"
+// @homepage        "https://github.com/sherryyuechiu/GreasyMonkeyScripts"
+// @require         https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.js
+// @grant           none
+// ==/UserScript==
 
-    (function () {
-        'use strict';
+(function () {
+    'use strict';
 
-        // 動態載入 CSS 的函式
-        function loadCSS(url) {
-            var link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            link.href = url;
-            document.head.appendChild(link);
+    // 動態載入 CSS 的函式
+    function loadCSS(url) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        document.head.appendChild(link);
+    }
+    loadCSS('https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css');
+
+    // 顯示 toast 的函式
+    function showToast(message) {
+        Toastify({
+            text: message,
+            duration: 6000,
+            style: {
+                textAlign: 'left',
+                width: '20em',
+                height: 'auto',
+                wordWrap: 'break-word',
+            }
+        }).showToast();
+    }
+
+    // 初始的 dataLayer 長度
+    let previousLength = window.dataLayer ? window.dataLayer.length : 0;
+
+    // 將 JSON 物件格式化，每個屬性顯示在新的一行
+    function formatJsonObject(jsonObject) {
+        let formattedString = '';
+
+        // 優先顯示 event 屬性
+        if (jsonObject.hasOwnProperty('event')) {
+            formattedString += `event: ${JSON.stringify(jsonObject['event'])}\n`;
         }
-        loadCSS('https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.min.css');
 
-        // 顯示 toast 的函式
-        function showToast(message) {
-            Toastify({
-                text: message,
-                duration: 6000,
-                style: {
-                    textAlign: 'left',
-                    width: '20em',
-                    height: 'auto',
-                    wordWrap: 'break-word',
+        // 優先顯示 page 屬性
+        if (jsonObject.hasOwnProperty('page')) {
+            formattedString += `page: ${JSON.stringify(jsonObject['page'])}\n`;
+        }
+
+        for (const key in jsonObject) {
+            if (jsonObject.hasOwnProperty(key) && key !== 'event' && key !== 'page') {
+                let value = jsonObject[key];
+                if ((key === 'after' || key === 'before') && typeof value === 'boolean') {
+                    value = value ? '⭕' : '❌';
                 }
-            }).showToast();
+                formattedString += `${key}: ${JSON.stringify(value)}\n`;
+            }
         }
 
-        // 初始的 dataLayer 長度
-        let previousLength = window.dataLayer ? window.dataLayer.length : 0;
+        return formattedString;
+    }
 
-        // 將 JSON 物件格式化，每個屬性顯示在新的一行
-        function formatJsonObject(jsonObject) {
-            let formattedString = '';
-
-            // 優先顯示 event 屬性
-            if (jsonObject.hasOwnProperty('event')) {
-                formattedString += `event: ${JSON.stringify(jsonObject['event'])}\n`;
-            }
-
-            // 優先顯示 page 屬性
-            if (jsonObject.hasOwnProperty('page')) {
-                formattedString += `page: ${JSON.stringify(jsonObject['page'])}\n`;
-            }
-
-            for (const key in jsonObject) {
-                if (jsonObject.hasOwnProperty(key) && key !== 'event' && key !== 'page') {
-                    let value = jsonObject[key];
-                    if ((key === 'after' || key === 'before') && typeof value === 'boolean') {
-                        value = value ? '⭕' : '❌';
-                    }
-                    formattedString += `${key}: ${JSON.stringify(value)}\n`;
-                }
-            }
-
-            return formattedString;
-        }
-
-        function checkDataLayer() {
-            if (window.dataLayer) {
-                let currentLength = window.dataLayer.length;
-                if (currentLength > previousLength) {
-                    // 新事件被加入
-                    let newEvent = window.dataLayer[window.dataLayer.length - 1];
-                    if (newEvent && newEvent.event) {
-                        const filteredEvent = {};
-                        for (const key in newEvent) {
-                            if (!key.startsWith('gtm.') && key !== 'gameId') {
-                                filteredEvent[key] = newEvent[key];
-                            }
+    function checkDataLayer() {
+        if (window.dataLayer) {
+            let currentLength = window.dataLayer.length;
+            if (currentLength > previousLength) {
+                // 新事件被加入
+                let newEvent = window.dataLayer[window.dataLayer.length - 1];
+                if (newEvent && newEvent.event) {
+                    const filteredEvent = {};
+                    for (const key in newEvent) {
+                        if (!key.startsWith('gtm.') && key !== 'gameId') {
+                            filteredEvent[key] = newEvent[key];
                         }
-
-                        showToast(formatJsonObject(filteredEvent));
                     }
-                } else if (currentLength < previousLength) {
-                    showToast('dataLayer被重置了');
-                }
-                previousLength = currentLength;
-            }
-        }
 
-        // 每秒檢查一次 dataLayer
-        setInterval(checkDataLayer, 100);
-    })();
+                    showToast(formatJsonObject(filteredEvent));
+                }
+            } else if (currentLength < previousLength) {
+                showToast('dataLayer被重置了');
+            }
+            previousLength = currentLength;
+        }
+    }
+
+    // 每秒檢查一次 dataLayer
+    setInterval(checkDataLayer, 100);
+})();
